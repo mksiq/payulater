0f28bd6a9ecd893740ae220621d3e3ea9aeca0d1
mksiq <maickelsiqueira@gmail.com>
2020-05-03 21:10:01 -0400

    Spring boot started

---
+++ src/main/java/com/mck/payulater/PayulaterApplication.java
@@ -0,0 +1,13 @@
+package com.mck.payulater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayulaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayulaterApplication.class, args);
+	}
+
+}
